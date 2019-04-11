b16119d40e2dd23fbdee34324d5f1e15312763bf
ThunderDev1 <sgranger1@gmail.com>
2019-04-11 20:59:17 +0200

    Added TypeScript support

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const App = () => {
+  return <div>React Typescript Parcel</div>;
+}
+
+export default App;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-const App = () => {
-  return <div>Yolo</div>;
-}
-
-render(<App />, document.getElementById('root'));
