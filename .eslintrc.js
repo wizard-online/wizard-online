d54639c10d2182c999a5d21939580c5af86d5eea
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-24 23:40:19 +0100

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,49 @@
+// based on https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['prettier', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  plugins: ["@typescript-eslint", "jest"],
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    jsx: true,
+    sourceType: "module",
+    useJSXTextNode: true,
+    // project: "./tsconfig.eslint.json",
+  },
+
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
   rules: {
-    indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    'react/jsx-uses-vars': 'error'
-  }
-}
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    "react/jsx-filename-extension": "off",
+    // Use function hoisting to improve code readability
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    "@typescript-eslint/no-implied-eval": "off",
+    "@typescript-eslint/no-throw-literal": "off",
+  },
+};
