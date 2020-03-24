dccc09ac63d0f9932c5e734a39f93d2c4f45c216
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-24 20:49:50 +0100

    cleanup prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  printWidth: 140,
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  jsxSingleQuote: false,
-  bracketSpacing: false,
-  jsxBracketSameLine: false,
-  trailingComma: 'es5',
+  trailingComma: "es5"
 };
