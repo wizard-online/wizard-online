b21155c5d78ce30e95aade7515c4f73e198d7fb0
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-25 21:37:59 +0100

    fix babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-env", { corejs: 3, useBuiltIns: "usage" }],
     "@babel/preset-typescript",
     "@babel/react",
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    { corejs: 3, regenerator: true },
   ],
 };
