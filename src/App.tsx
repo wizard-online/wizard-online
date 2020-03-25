fd9e872d98ddc59172805a6c73d75f90f1ef8039
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-25 03:49:28 +0100

    show Game component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-import { HashRouter as Router } from "react-router-dom";
-import { Routes } from "./Routes";
-import { Nav } from "./components/Nav";
+import { Game } from "./Game";
 
-export const App: React.FC<{}> = () => (
-  <Router>
-    <>
-      <h1>TEST TITLE</h1>
-      <Nav />
-      <Routes />
-    </>
-  </Router>
-);
+export const App: React.FC<{}> = () => <Game />;
