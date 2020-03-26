01caa85b4852d6bf0ac71383ae4e26685065da38
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-26 21:34:02 +0100

    enhance PlayCard

--- src/gameboard/components/PlayCard.tsx
+++ src/gameboard/components/PlayCard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { Box } from "@material-ui/core";
 import { Card, Rank, Suit } from "../../boardgame/entities/cards";
 
 export interface PlayCardProps {
-  card: Card;
+  card: Card | null;
+  playable: boolean;
+  onClick?: () => void;
 }
 
 // css colors
@@ -14,6 +17,58 @@ enum PlayCardColor {
   Red = "red",
   Yellow = "yellow",
 }
+
+export const PlayCard: React.FC<PlayCardProps> = ({
+  card,
+  playable = true,
+  onClick = () => {},
+}) => {
+  if (!card) {
+    return <StaticCardBox>&#10060;</StaticCardBox>;
+  }
+  const color = getColor(card);
+  const label = getLabel(card);
+
+  const cardContent = (
+    <CardText color={color}>
+      {label}
+      {playable && "P"}
+    </CardText>
+  );
+
+  if (playable)
+    return <PlayableCardBox onClick={onClick}>{cardContent}</PlayableCardBox>;
+  return <NonPlayableCardBox>{cardContent}</NonPlayableCardBox>;
+};
+
+const StaticCardBox = styled(Box)`
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 66px;
+`;
+
+const PlayableCardBox = styled(StaticCardBox)`
+  cursor: pointer;
+  transition: transform 0.3s;
+  &:hover {
+    transform: translate(0, -10px);
+  }
+`;
+
+const NonPlayableCardBox = styled(StaticCardBox)`
+  background-color: lightgrey;
+`;
+
+const CardText = styled.span<{ color: PlayCardColor }>`
+  color: ${({ color }) => color};
+  font-weight: bold;
+  font-size: 14px;
+`;
 
 function getColor({ rank, suit }: Card): PlayCardColor {
   if (rank === Rank.Z || rank === Rank.N) return PlayCardColor.Black;
@@ -42,14 +97,3 @@ function getLabel({ rank }: Card): string {
       return rank.toString();
   }
 }
-
-export const PlayCard: React.FC<PlayCardProps> = ({ card }) => {
-  const color = getColor(card);
-  const label = getLabel(card);
-
-  return <CardText color={color}>{label}</CardText>;
-};
-
-const CardText = styled.span<{ color: PlayCardColor }>`
-  color: ${({ color }) => color};
-`;
