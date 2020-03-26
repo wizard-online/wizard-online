b03ca0a161cde766372e497350ad59ebba87893a
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-26 01:22:43 +0100

    add describe to predict test

--- src/boardgame/phases/predict.test.ts
+++ src/boardgame/phases/predict.test.ts
@@ -33,66 +33,78 @@ function generate({
   };
 }
 
-test("invalid when prediction below 0", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  expect(predict.moves?.predictNumberOfTricks(g, ctx, -1)).toBe(INVALID_MOVE);
-});
+describe("predict", () => {
+  test("invalid when prediction below 0", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
+    expect(predict.moves?.predictNumberOfTricks(g, ctx, -1)).toBe(INVALID_MOVE);
+  });
 
-test("invalid when prediction above number of cards", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  expect(predict.moves?.predictNumberOfTricks(g, ctx, 4)).toBe(INVALID_MOVE);
-});
+  test("invalid when prediction above number of cards", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
+    expect(predict.moves?.predictNumberOfTricks(g, ctx, 4)).toBe(INVALID_MOVE);
+  });
 
-test("not invalid when prediction is 0", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  expect(predict.moves?.predictNumberOfTricks(g, ctx, 0)).not.toBe(
-    INVALID_MOVE
-  );
-});
-
-test("not invalid when prediction equals number of cards and not last player", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  expect(predict.moves?.predictNumberOfTricks(g, ctx, 3)).not.toBe(
-    INVALID_MOVE
-  );
-});
-
-test("invalid when last player's prediction makes it even", () => {
-  {
-    const { ctx, g } = generate({ score: [1, 0, 1, null] });
-    expect(predict.moves?.predictNumberOfTricks(g, ctx, 1)).toBe(INVALID_MOVE);
-  }
-  {
-    const { ctx, g } = generate({ score: [2, 0, 1, null] });
-    expect(predict.moves?.predictNumberOfTricks(g, ctx, 0)).toBe(INVALID_MOVE);
-  }
-});
-
-test("not invalid when last player's prediction makes it even in first round", () => {
-  {
-    const { ctx, g } = generate({ score: [1, 0, 0, null], numCardsOnHand: 1 });
+  test("not invalid when prediction is 0", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
     expect(predict.moves?.predictNumberOfTricks(g, ctx, 0)).not.toBe(
       INVALID_MOVE
     );
-  }
-  {
-    const { ctx, g } = generate({ score: [0, 0, 0, null], numCardsOnHand: 1 });
-    expect(predict.moves?.predictNumberOfTricks(g, ctx, 1)).not.toBe(
+  });
+
+  test("not invalid when prediction equals number of cards and not last player", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
+    expect(predict.moves?.predictNumberOfTricks(g, ctx, 3)).not.toBe(
       INVALID_MOVE
     );
-  }
-});
+  });
 
-test("sets the corresponding score", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  predict.moves?.predictNumberOfTricks(g, ctx, 2);
-  expect(g.score[1]).toBe(2);
-});
+  test("invalid when last player's prediction makes it even", () => {
+    {
+      const { ctx, g } = generate({ score: [1, 0, 1, null] });
+      expect(predict.moves?.predictNumberOfTricks(g, ctx, 1)).toBe(
+        INVALID_MOVE
+      );
+    }
+    {
+      const { ctx, g } = generate({ score: [2, 0, 1, null] });
+      expect(predict.moves?.predictNumberOfTricks(g, ctx, 0)).toBe(
+        INVALID_MOVE
+      );
+    }
+  });
 
-test("ends the turn", () => {
-  const { ctx, g } = generate({ score: [1, null, null, null] });
-  const mockEndTurn = jest.fn();
-  ctx.events!.endTurn = mockEndTurn;
-  predict.moves?.predictNumberOfTricks(g, ctx, 2);
-  expect(mockEndTurn).toBeCalled();
+  test("not invalid when last player's prediction makes it even in first round", () => {
+    {
+      const { ctx, g } = generate({
+        score: [1, 0, 0, null],
+        numCardsOnHand: 1,
+      });
+      expect(predict.moves?.predictNumberOfTricks(g, ctx, 0)).not.toBe(
+        INVALID_MOVE
+      );
+    }
+    {
+      const { ctx, g } = generate({
+        score: [0, 0, 0, null],
+        numCardsOnHand: 1,
+      });
+      expect(predict.moves?.predictNumberOfTricks(g, ctx, 1)).not.toBe(
+        INVALID_MOVE
+      );
+    }
+  });
+
+  test("sets the corresponding score", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
+    predict.moves?.predictNumberOfTricks(g, ctx, 2);
+    expect(g.score[1]).toBe(2);
+  });
+
+  test("ends the turn", () => {
+    const { ctx, g } = generate({ score: [1, null, null, null] });
+    const mockEndTurn = jest.fn();
+    ctx.events!.endTurn = mockEndTurn;
+    predict.moves?.predictNumberOfTricks(g, ctx, 2);
+    expect(mockEndTurn).toBeCalled();
+  });
 });
