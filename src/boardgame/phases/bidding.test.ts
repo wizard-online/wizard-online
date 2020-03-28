f384ba05e1195b959a9b309c0708050cb05b775d
janKir <9143877+janKir@users.noreply.github.com>
2020-03-28 15:42:18 +0100

    fix typing errors in tests

--- src/boardgame/phases/bidding.test.ts
+++ src/boardgame/phases/bidding.test.ts
@@ -1,7 +1,7 @@
 import { Ctx } from "boardgame.io";
 import { INVALID_MOVE } from "boardgame.io/core";
 
-import { bidding } from "./bidding";
+import { bid } from "./bidding";
 import { G } from "../G";
 import { generateCtx } from "../util/ctx.util";
 
@@ -45,32 +45,32 @@ function generate({
 describe("predict", () => {
   test("invalid when prediction below 0", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
-    expect(bidding.moves?.bid(g, ctx, -1)).toBe(INVALID_MOVE);
+    expect(bid(g, ctx, -1)).toBe(INVALID_MOVE);
   });
 
   test("invalid when prediction above number of cards", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
-    expect(bidding.moves?.bid(g, ctx, 4)).toBe(INVALID_MOVE);
+    expect(bid(g, ctx, 4)).toBe(INVALID_MOVE);
   });
 
   test("not invalid when prediction is 0", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
-    expect(bidding.moves?.bid(g, ctx, 0)).not.toBe(INVALID_MOVE);
+    expect(bid(g, ctx, 0)).not.toBe(INVALID_MOVE);
   });
 
   test("not invalid when prediction equals number of cards and not last player", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
-    expect(bidding.moves?.bid(g, ctx, 3)).not.toBe(INVALID_MOVE);
+    expect(bid(g, ctx, 3)).not.toBe(INVALID_MOVE);
   });
 
   test("invalid when last player's prediction makes it even", () => {
     {
       const { ctx, g } = generate({ bids: [1, 0, 1, null] });
-      expect(bidding.moves?.bid(g, ctx, 1)).toBe(INVALID_MOVE);
+      expect(bid(g, ctx, 1)).toBe(INVALID_MOVE);
     }
     {
       const { ctx, g } = generate({ bids: [2, 0, 1, null] });
-      expect(bidding.moves?.bid(g, ctx, 0)).toBe(INVALID_MOVE);
+      expect(bid(g, ctx, 0)).toBe(INVALID_MOVE);
     }
   });
 
@@ -80,20 +80,20 @@ describe("predict", () => {
         bids: [1, 0, 0, null],
         numCards: 1,
       });
-      expect(bidding.moves?.bid(g, ctx, 0)).not.toBe(INVALID_MOVE);
+      expect(bid(g, ctx, 0)).not.toBe(INVALID_MOVE);
     }
     {
       const { ctx, g } = generate({
         bids: [0, 0, 0, null],
         numCards: 1,
       });
-      expect(bidding.moves?.bid(g, ctx, 1)).not.toBe(INVALID_MOVE);
+      expect(bid(g, ctx, 1)).not.toBe(INVALID_MOVE);
     }
   });
 
   test("sets the corresponding score", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
-    bidding.moves?.bid(g, ctx, 2);
+    bid(g, ctx, 2);
     expect(g.round!.bids[1]).toBe(2);
   });
 
@@ -101,7 +101,7 @@ describe("predict", () => {
     const { ctx, g } = generate({ bids: [1, null, null, null] });
     const mockEndTurn = jest.fn();
     ctx.events!.endTurn = mockEndTurn;
-    bidding.moves?.bid(g, ctx, 2);
+    bid(g, ctx, 2);
     expect(mockEndTurn).toBeCalled();
   });
 });
