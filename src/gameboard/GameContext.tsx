ca6f4f49f484497c1af929e458fe902c5e150725
janKir <9143877+janKir@users.noreply.github.com>
2020-03-29 19:27:38 +0200

    remove unused field from AppState interface

--- src/gameboard/GameContext.tsx
+++ src/gameboard/GameContext.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { GameState } from "./GameState";
-import { PlayerID } from "../boardgame/entities/players";
 
 export interface AppState {
   gamestate?: GameState;
-  clientID?: PlayerID;
 }
 
 export const GameContext = React.createContext<AppState>({});
