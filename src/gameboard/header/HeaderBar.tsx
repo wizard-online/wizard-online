98123ffc0c0f8ff3c2f7a9576eddbeb80121052b
janKir <9143877+janKir@users.noreply.github.com>
2020-03-31 18:33:42 +0200

    rework HeaderBar

--- src/gameboard/header/HeaderBar.tsx
+++ src/gameboard/header/HeaderBar.tsx
@@ -3,20 +3,30 @@ import { AppBar, Box, Toolbar } from "@material-ui/core";
 import styled from "styled-components";
 
 import { useGameState } from "../GameContext";
+import { maxCards } from "../../boardgame/entities/players";
 
 export const HeaderBar: React.FC = () => {
   const {
-    wizardState: { phase, currentPlayer, numCards, numPlayers },
+    wizardState: { numCards, numPlayers, round },
   } = useGameState();
   return (
     <AppBar position="sticky">
       <Toolbar>
         <h1>Wizard Online</h1>
         <SpaceFill />
-        <InfoItem>Phase: {phase}</InfoItem>
-        <InfoItem>PlayerId: {currentPlayer}</InfoItem>
-        <InfoItem>Runde: {numCards}</InfoItem>
-        <InfoItem>#Spieler: {numPlayers}</InfoItem>
+        {round && round.bidsMismatch !== undefined && (
+          <InfoItem>
+            {round.bidsMismatch > 0 && "+"}
+            {round.bidsMismatch} Stiche Abweichung{" "}
+            {round.bidsMismatch > 0 && "(offensiv)"}
+            {round.bidsMismatch < 0 && "(defensiv)"}
+            {round.bidsMismatch === 0 && "(ausgeglichen)"}
+          </InfoItem>
+        )}
+        <InfoItem>
+          Runde {numCards} / {maxCards(numPlayers)}
+        </InfoItem>
+        <InfoItem>{numPlayers} Spieler</InfoItem>
       </Toolbar>
     </AppBar>
   );
@@ -27,5 +37,5 @@ const SpaceFill = styled.div`
 `;
 
 const InfoItem = styled(Box)`
-  margin: 8px;
+  margin: 15px;
 `;
