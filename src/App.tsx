06d8c0dd9b694e07dbbeba794649d0c229d03b80
janKir <9143877+janKir@users.noreply.github.com>
2020-04-02 02:42:37 +0200

    enhance Client with redux logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
-import { Lobby } from "boardgame.io/react";
+import { Lobby, Client } from "boardgame.io/react";
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
 import { wizardGameConfig } from "./boardgame/game";
 import { WizardBoard } from "./gameboard/WizardBoard";
+
+// en-/disable console logging of redux actions
+const REDUX_LOGGER = false;
 
 export const App: React.FC<{}> = () => (
   <Lobby
     gameServer="http://localhost:8000"
     lobbyServer="http://localhost:8000"
-    gameComponents={[{ game: wizardGameConfig, board: WizardBoard }]}
+    gameComponents={[
+      {
+        game: wizardGameConfig,
+        board: WizardBoard,
+      },
+    ]}
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    clientFactory={(opts: any) => {
+      return Client({
+        enhancer: REDUX_LOGGER ? applyMiddleware(logger) : undefined,
+        ...opts,
+      });
+    }}
   />
 );
