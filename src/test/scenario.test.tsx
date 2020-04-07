af0300b01e548ea1c986c47e36946a21340fc67f
janKir <9143877+janKir@users.noreply.github.com>
2020-04-08 00:19:15 +0200

    wip scenario test

--- src/test/scenario.test.tsx
+++ src/test/scenario.test.tsx
@@ -12,6 +12,7 @@ import {
   getByTestId,
   queryByLabelText,
   queryByTestId,
+  act,
 } from "@testing-library/react";
 
 import { Client } from "boardgame.io/react";
@@ -23,9 +24,17 @@ import shuffle from "lodash/shuffle";
 import range from "lodash/range";
 import { wizardGameConfig } from "../game/game";
 import { WizardBoard } from "../ui/WizardBoard";
-import { Suit, Card } from "../game/entities/cards";
+import { Suit, Card, Rank } from "../game/entities/cards";
 import { getCardLabel } from "../game/entities/cards.utils";
 import { PlayerID, NumPlayers } from "../game/entities/players";
+
+const C = Card;
+const B = Suit.Blue;
+const G = Suit.Green;
+const R = Suit.Red;
+const Y = Suit.Yellow;
+const { N } = Rank;
+const { Z } = Rank;
 
 jest.mock("lodash/random");
 jest.mock("lodash/shuffle");
@@ -44,8 +53,8 @@ let renderResult: RenderResult;
 let clients: HTMLElement[];
 beforeAll(() => {
   // mock initial dealer selection
-  randomMock.mockReturnValueOnce(2);
-  shuffleMock.mockReturnValueOnce([]);
+  randomMock.mockReturnValue(0);
+  shuffleMock.mockReturnValue([]);
 
   const ids = [0, 1, 2, 3];
 
@@ -109,6 +118,9 @@ function playMove(playerID: PlayerID, card: Card): void {
   // test card added on table
   const tablePlay = getByTestId(clients[playerID], "table-play");
   expect(queryByLabelText(tablePlay, getCardLabel(card))).toBeInTheDocument();
+  expect(
+    queryByLabelText(clientHand, getCardLabel(card))
+  ).not.toBeInTheDocument();
 }
 
 interface RoundScenario {
@@ -151,9 +163,15 @@ function testIsTurn(playerID: PlayerID, numPlayers: number): void {
   range(0, numPlayers)
     .filter((pID) => pID !== playerID)
     .forEach((pID) => {
-      expect(
-        queryByText(clients[pID], /du bist am zug/i)
-      ).not.toBeInTheDocument();
+      try {
+        expect(
+          queryByText(clients[pID], /du bist am zug/i)
+        ).not.toBeInTheDocument();
+      } catch (error) {
+        console.log(playerID, pID);
+        console.log(prettyDOM(clients[pID]));
+        throw new Error(error);
+      }
     });
   expect(queryByText(clients[playerID], /du bist am zug/i)).toBeInTheDocument();
 }
@@ -162,9 +180,14 @@ function testDealer({ dealer, numPlayers }: RoundScenario): void {
   range(0, numPlayers)
     .filter((playerID) => playerID !== dealer)
     .forEach((playerID) => {
-      expect(
-        queryByText(clients[playerID], /du bist am zug/i)
-      ).not.toBeInTheDocument();
+      try {
+        expect(
+          queryByText(clients[playerID], /du bist am zug/i)
+        ).not.toBeInTheDocument();
+      } catch (error) {
+        console.log(dealer, playerID, numPlayers);
+        throw new Error(error);
+      }
     });
   expect(queryByText(clients[dealer], /du bist am zug/i)).toBeInTheDocument();
 }
@@ -210,14 +233,14 @@ test("4 four players game", () => {
   const round1Scenario: RoundScenario = {
     numPlayers: 4,
     numCards: 1,
-    dealer: 2,
+    dealer: 0,
     moves: {
-      0: { bid: 1, play: [Card(Suit.Red, 7)] },
-      1: { bid: 0, play: [Card(Suit.Blue, 3)] },
-      2: { bid: 0, play: [Card(Suit.Red, 9)] },
-      3: { bid: 1, play: [Card(Suit.Green, 11)] },
+      0: { bid: 0, play: [C(Y, 7)] },
+      1: { bid: 1, play: [C(R, 5)] },
+      2: { bid: 0, play: [C(R, 12)] },
+      3: { bid: 0, play: [C(G, 10)] },
     },
-    trumpCard: Card(Suit.Blue, 4),
+    trumpCard: C(B, N),
   };
   const { numPlayers } = round1Scenario;
   let currentPlayer: PlayerID = round1Scenario.dealer;
@@ -232,19 +255,37 @@ test("4 four players game", () => {
 
   // handout
   handoutMove(currentPlayer);
-  currentPlayer = nextPlayer(currentPlayer, numPlayers);
   testCorrectHandout(round1Scenario);
+
+  currentPlayer = nextPlayer(currentPlayer, numPlayers);
 
   // do bidding round
   doRound(currentPlayer, numPlayers, (playerID) => {
     const { bid } = round1Scenario.moves[playerID];
     bidMove(playerID, bid, numPlayers);
   });
-  testIsTurn(3, round1Scenario.numPlayers);
+  testIsTurn(currentPlayer, round1Scenario.numPlayers);
 
   // do playing
-  doRound(currentPlayer, numPlayers, (playerID) =>
-    playMove(playerID, round1Scenario.moves[playerID].play[0])
-  );
-  testIsTurn(3, round1Scenario.numPlayers);
+  doRound(currentPlayer, numPlayers, (playerID) => {
+    playMove(playerID, round1Scenario.moves[playerID].play[0]);
+    console.log(
+      `played player ${playerID}, card: ${round1Scenario.moves[playerID].play[0]}`
+    );
+  });
+
+  // round 2
+  const round2Scenario: RoundScenario = {
+    numPlayers: 4,
+    numCards: 2,
+    dealer: 3,
+    moves: {
+      0: { bid: 1, play: [C(R, 7)] },
+      1: { bid: 0, play: [C(B, 3)] },
+      2: { bid: 0, play: [C(R, 9)] },
+      3: { bid: 1, play: [C(G, 11)] },
+    },
+    trumpCard: C(B, 4),
+  };
+  testIsTurn(currentPlayer, numPlayers);
 });
