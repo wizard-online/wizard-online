9c16a490614c76ce795cc2c6e94476a47c332791
janKir <9143877+janKir@users.noreply.github.com>
2020-04-12 21:12:53 +0200

    update server types

--- src/boardgame.io.server.d.ts
+++ src/boardgame.io.server.d.ts
@@ -1,6 +1,7 @@
 declare module "boardgame.io/server" {
   export interface Server {
-    run(port: number, onStart: () => void): void;
+    run(port: number, onStart: () => void): Promise<any>;
+    kill(args: any): void;
   }
   export function Server(options: object): Server;
 }
