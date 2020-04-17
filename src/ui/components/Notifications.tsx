512aa33113c17cff1fb5adea453b58306555af0d
janKir <9143877+janKir@users.noreply.github.com>
2020-04-17 15:56:24 +0200

    create Notifications component

---
+++ src/ui/components/Notifications.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useRef } from "react";
+import { Snackbar, SnackbarCloseReason } from "@material-ui/core";
+import random from "lodash/random";
+
+interface Notification {
+  message: string;
+}
+
+interface NotificationWithKey extends Notification {
+  key: number;
+}
+
+export const Notifications: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState<Notification | undefined>(
+    undefined
+  );
+  const queue = useRef<NotificationWithKey[]>([]);
+  function processQueue(): void {
+    if (queue.current.length > 0) {
+      setNotification(queue.current.shift());
+      setShow(true);
+    }
+  }
+  function showNotification(newNotification: Notification): void {
+    queue.current.push({ ...newNotification, key: new Date().getTime() });
+    if (show) {
+      setShow(false);
+    } else {
+      processQueue();
+    }
+  }
+  // eslint-disable-next-line no-empty-pattern
+  function handleClose({}, reason: SnackbarCloseReason): void {
+    if (reason === "clickaway") {
+      return;
+    }
+    setShow(false);
+  }
+  function handleExited(): void {
+    processQueue();
+  }
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() =>
+          showNotification({
+            message: `Hallo ${random(1, 100)}!!!`,
+          })
+        }
+      >
+        Notification
+      </button>
+      <Snackbar
+        open={show}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        onExited={handleExited}
+        message={notification ? notification.message : undefined}
+      />
+    </div>
+  );
+};
