56ba121e4b61180e4b6e4c27d3f747b5c4320020
janKir <9143877+janKir@users.noreply.github.com>
2020-04-18 18:27:15 +0200

    implement round complete message

--- src/ui/hooks/notify-round-complete.ts
+++ src/ui/hooks/notify-round-complete.ts
@@ -2,19 +2,27 @@ import { useCallback } from "react";
 import { useGameEventHandler } from "./game-event-handler";
 import { GameEvent } from "../util/game-events";
 import { useNotify } from "../NotificationsProvider";
-// import { useGameState } from "../GameContext";
+import { GameState } from "../GameState";
 
 export function useNotifyRoundComplete(): void {
-  // const {
-  //   wizardState: { round, trick },
-  //   gameMetadata,
-  // } = useGameState();
   const notify = useNotify();
 
-  const handleRoundComplete = useCallback(() => {
-    console.log("round completed!");
-    notify({ message: "round completed!" });
-  }, [notify]);
+  const handleRoundComplete = useCallback(
+    ({ wizardState: { scorePad }, clientID }: GameState) => {
+      if (scorePad.length > 0) {
+        const { numCards, playerScores } = scorePad[scorePad.length - 1];
+
+        let message = `Runde ${numCards} beendet.`;
+        if (clientID >= 0) {
+          const { score } = playerScores[clientID];
+          message += ` ${score} Punkte.`;
+        }
+
+        notify({ message });
+      }
+    },
+    [notify]
+  );
 
   useGameEventHandler(GameEvent.RoundComplete, handleRoundComplete);
 }
