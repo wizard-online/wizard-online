7bb55a8f7bc8618348d35d03190cb1195d54852a
janKir <9143877+janKir@users.noreply.github.com>
2020-04-21 17:44:37 +0200

    cleanup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,13 @@
 import React from "react";
 
-import { Lobby, Client } from "boardgame.io/react";
-import { applyMiddleware } from "redux";
-import logger from "redux-logger";
-import styled from "styled-components";
 import { BrowserRouter } from "react-router-dom";
-import { wizardGameConfig } from "./game/game";
-import { WizardBoard } from "./ui/WizardBoard";
 import { WizardLobby } from "./ui/lobby/WizardLobby";
 import { ProfileProvider } from "./ui/ProfileProvider";
 
-// en-/disable console logging of redux actions
-const REDUX_LOGGER = false;
-
 export const App: React.FC<{}> = () => (
-  // <StyledLobby>
-  //   <Lobby
-  //     gameServer={process.env.API_URL}
-  //     lobbyServer={process.env.API_URL}
-  //     gameComponents={[
-  //       {
-  //         game: wizardGameConfig,
-  //         board: WizardBoard,
-  //       },
-  //     ]}
-  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     clientFactory={(opts: any) => {
-  //       return Client({
-  //         enhancer: REDUX_LOGGER ? applyMiddleware(logger) : undefined,
-  //         ...opts,
-  //       });
-  //     }}
-  //   />{" "}
-  // </StyledLobby>
   <BrowserRouter>
     <ProfileProvider>
       <WizardLobby />
     </ProfileProvider>
   </BrowserRouter>
 );
-
-const StyledLobby = styled.div`
-  padding: 10px;
-  & #lobby-view {
-    padding: 0 !important;
-  }
-  & .hidden {
-    display: none;
-  }
-`;
