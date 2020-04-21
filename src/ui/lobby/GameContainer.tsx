c80c2744bc0918786f737fcb6fd8ee3c4ddaa1d4
janKir <9143877+janKir@users.noreply.github.com>
2020-04-21 12:12:02 +0200

    create PlayGame component

--- src/ui/lobby/GameContainer.tsx
+++ src/ui/lobby/GameContainer.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { EnterGame } from "./EnterGame";
+import { PlayGame } from "./PlayGame";
 
 export const GameContainer: React.FC = () => {
   const { gameID } = useParams<{ gameID: string }>();
   const [playing, setPlaying] = useState(false);
 
   if (playing) {
-    return <div>Playing...</div>;
+    return <PlayGame />;
   }
   return <EnterGame gameID={gameID} startGame={() => setPlaying(true)} />;
 };
---
+++ src/ui/lobby/PlayGame.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PlayGame: React.FC = () => {
+  return <div>Playing...</div>;
+};
