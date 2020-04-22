a9ed6e77e9c91f5b80d26d9df44062221b630912
janKir <9143877+janKir@users.noreply.github.com>
2020-04-22 02:40:29 +0200

    add icon to profile header element

--- src/ui/hooks/profile-header-element.tsx
+++ src/ui/hooks/profile-header-element.tsx
@@ -1,7 +1,21 @@
+import React, { useMemo } from "react";
+import { Icon } from "@material-ui/core";
 import { useProfile } from "../ProfileProvider";
 import { useHeaderElement } from "../header/HeaderElementsProvider";
 
 export function useProfileHeaderElement(): void {
   const { name } = useProfile();
-  useHeaderElement("profile", 1, name);
+  useHeaderElement(
+    "profile",
+    1,
+    useMemo(
+      () => (
+        <>
+          <Icon>account_circle</Icon>
+          {name}
+        </>
+      ),
+      [name]
+    )
+  );
 }
