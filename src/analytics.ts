171a722e15fb575ed40d68e0e426016234b59b8a
janKir <9143877+janKir@users.noreply.github.com>
2020-04-26 12:02:43 +0200

    simplify pageview call

--- src/analytics.ts
+++ src/analytics.ts
@@ -3,6 +3,15 @@ import ReactGA from "react-ga";
 export function initializeGA(): void {
   if (process.env.ANALYTICS_ID) {
     ReactGA.initialize(process.env.ANALYTICS_ID);
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    pageview();
   }
+}
+
+export function pageview(location?: { pathname: string }): void {
+  const pathname = location?.pathname ?? window.location.pathname;
+  let analyticsPath = pathname;
+  if (pathname.match(/games\/[\w-]+/i)) {
+    analyticsPath = "/games/game-id";
+  }
+  ReactGA.pageview(analyticsPath);
 }
--- src/ui/lobby/LobbyRouter.tsx
+++ src/ui/lobby/LobbyRouter.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import ReactGA from "react-ga";
 import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 import { CreateGame } from "./CreateGame";
 import { GameContainer } from "./GameContainer";
 import { useProfileHeaderElement } from "../hooks/profile-header-element";
 import { ListGames } from "./ListGames";
 import { useFeedbackHeaderElement } from "../hooks/feedback-header-element";
+import { pageview } from "../../analytics";
 
 export const LobbyRouter: React.FC = () => {
   useProfileHeaderElement();
@@ -13,13 +13,7 @@ export const LobbyRouter: React.FC = () => {
 
   const history = useHistory();
   useEffect(() => {
-    const unlisten = history.listen((location) => {
-      let analyticsPath = location.pathname;
-      if (location.pathname.match(/games\/[\w-]+/i)) {
-        analyticsPath = "/games/game-id";
-      }
-      ReactGA.pageview(analyticsPath);
-    });
+    const unlisten = history.listen(pageview);
     return () => unlisten();
   }, [history]);
 
