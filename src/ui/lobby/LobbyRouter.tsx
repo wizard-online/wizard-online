9748c255b11fd08183a29e8793bfa8e20cc1236c
janKir <9143877+janKir@users.noreply.github.com>
2020-04-26 03:31:28 +0200

    listen to history changes and fire GA pageviews

--- src/ui/lobby/LobbyRouter.tsx
+++ src/ui/lobby/LobbyRouter.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 import { CreateGame } from "./CreateGame";
 import { GameContainer } from "./GameContainer";
 import { useProfileHeaderElement } from "../hooks/profile-header-element";
@@ -9,22 +10,33 @@ import { useFeedbackHeaderElement } from "../hooks/feedback-header-element";
 export const LobbyRouter: React.FC = () => {
   useProfileHeaderElement();
   useFeedbackHeaderElement();
+
+  const history = useHistory();
+  useEffect(() => {
+    const unlisten = history.listen((location) => {
+      let analyticsPath = location.pathname;
+      if (location.pathname.match(/games\/[\w-]+/i)) {
+        analyticsPath = "/games/game-id";
+      }
+      ReactGA.pageview(analyticsPath);
+    });
+    return () => unlisten();
+  }, [history]);
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <CreateGame />
-        </Route>
-        <Route exact path="/games">
-          <ListGames />
-        </Route>
-        <Route path="/games/:gameID">
-          <GameContainer />
-        </Route>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <CreateGame />
+      </Route>
+      <Route exact path="/games">
+        <ListGames />
+      </Route>
+      <Route path="/games/:gameID">
+        <GameContainer />
+      </Route>
+      <Route path="*">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
   );
 };
