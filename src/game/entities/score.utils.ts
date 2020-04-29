0ac1b1f1d3e59ca09e8edd12572e212261682951
janKir <9143877+janKir@users.noreply.github.com>
2020-04-30 01:12:55 +0200

    refactor getLeader & co. functions to reduce redundancy

--- src/game/entities/score.utils.ts
+++ src/game/entities/score.utils.ts
@@ -34,6 +34,87 @@ export function calcRoundScore(
   };
 }
 
+/**
+ *
+ *
+ * @param {Score[][]} scoresByPlayer
+ * @returns {number[]} indeces of player cols with highest score
+ */
+export const getPlayersWithHighestScore = abstractGetPlayersWithMaxValue(
+  (scoresByPlayer) =>
+    scoresByPlayer.map(
+      (playerScores) => playerScores[playerScores.length - 1].total
+    )
+);
+
+export const getPlayersWithMostWonRounds = abstractGetPlayersWithMaxValue(
+  (scoresByPlayer) =>
+    scoresByPlayer.map((playerScores) =>
+      playerScores.reduce(
+        (sumWonRounds, score) =>
+          score.score > 0 ? sumWonRounds + 1 : sumWonRounds,
+        0
+      )
+    )
+);
+
+export const getPlayersWithHighestIndividualScore = abstractGetPlayersWithMaxValue(
+  (scoresByPlayer) =>
+    scoresByPlayer.map((playerScores) =>
+      Math.max(...playerScores.map((score) => score.score))
+    )
+);
+
+export const getPlayersWithMostAmountOfHighestIndividualScore = abstractGetPlayersWithMaxValue(
+  (scoresByPlayer) => {
+    const highestIndividualScores = scoresByPlayer.map((playerScores) =>
+      Math.max(...playerScores.map((score) => score.score))
+    );
+    const maxHighestIndividualScore = Math.max(...highestIndividualScores);
+
+    const amountsOfHighestIndividualScore = scoresByPlayer.map(
+      (playerScores) =>
+        playerScores.filter(
+          (score) => score.score === maxHighestIndividualScore
+        ).length
+    );
+    return amountsOfHighestIndividualScore;
+  }
+);
+
+export const getPlayersWithSecondHighestIndividualScore = abstractGetPlayersWithMaxValue(
+  (scoresByPlayer) => {
+    const highestIndividualScores = scoresByPlayer.map((playerScores) =>
+      Math.max(...playerScores.map((score) => score.score))
+    );
+    const maxHighestIndividualScore = Math.max(...highestIndividualScores);
+
+    const secondHighestIndividualScores = scoresByPlayer.map((playerScores) =>
+      Math.max(
+        ...playerScores
+          .map((score) => score.score)
+          .filter((score) => score !== maxHighestIndividualScore)
+      )
+    );
+    return secondHighestIndividualScores;
+  }
+);
+
+function abstractGetPlayersWithMaxValue(
+  calcValues: (scoresByPlayer: Score[][]) => number[]
+): (scoresByPlayer: Score[][]) => number[] {
+  return (scoresByPlayer: Score[][]): number[] => {
+    const values = calcValues(scoresByPlayer);
+    const maxValue = Math.max(...values);
+    const playersWithMaxValue = values
+      .map((value, i) => ({ i, value }))
+      .filter(({ value }) => value === maxValue)
+      .map(({ i }) => i);
+
+    return playersWithMaxValue;
+  };
+}
+
 export function getLeader(scorePad: ScorePad): PlayerID[] {
   if (scorePad.length === 0)
     throw new Error("cannot get leader of empty score pad");
@@ -41,208 +122,31 @@ export function getLeader(scorePad: ScorePad): PlayerID[] {
   let scoresByPlayer = transposeMatrix(
     scorePad.map((scoreRow) => scoreRow.playerScores)
   );
-  let players = scoresByPlayer.map((score, i) => i as PlayerID);
+  let players = scoresByPlayer.map((_score, i) => i as PlayerID);
 
-  // 1. players with highest score
-  const playerIndecesWithHighestScore = getPlayersWithHighestScore(
-    scoresByPlayer
-  );
-  players = players.filter((playerID, i) =>
-    playerIndecesWithHighestScore.includes(i)
-  );
-  if (players.length === 1) {
-    return players;
+  const rules = [
+    getPlayersWithHighestScore,
+    getPlayersWithMostWonRounds,
+    getPlayersWithHighestIndividualScore,
+    getPlayersWithMostAmountOfHighestIndividualScore,
+    getPlayersWithSecondHighestIndividualScore,
+  ];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const rule of rules) {
+    const playerIndeces = rule(scoresByPlayer);
+    players = players.filter((_playerID, i) => playerIndeces.includes(i));
+    if (players.length === 1) {
+      return players;
+    }
+    scoresByPlayer = scoresByPlayer.filter((_playerScores, i) =>
+      playerIndeces.includes(i)
+    );
   }
-
-  // 2. players with most won rounds
-  scoresByPlayer = scoresByPlayer.filter((playerScores, i) =>
-    playerIndecesWithHighestScore.includes(i)
-  );
-  const playerIndecesWithMostWonRounds = getPlayersWithMostWonRounds(
-    scoresByPlayer
-  );
-  players = players.filter((playerID, i) =>
-    playerIndecesWithMostWonRounds.includes(i)
-  );
-  if (players.length === 1) {
-    return players;
-  }
-
-  // 3. players with highest individual score
-  scoresByPlayer = scoresByPlayer.filter((playerScores, i) =>
-    playerIndecesWithMostWonRounds.includes(i)
-  );
-  const playerIndecesWithHighestIndividualScore = getPlayersWithHighestIndividualScore(
-    scoresByPlayer
-  );
-  players = players.filter((playerID, i) =>
-    playerIndecesWithHighestIndividualScore.includes(i)
-  );
-  if (players.length === 1) {
-    return players;
-  }
-
-  // 4. players with most amount of highest individual score
-  scoresByPlayer = scoresByPlayer.filter((playerScores, i) =>
-    playerIndecesWithHighestIndividualScore.includes(i)
-  );
-  const playerIndecesWithMostAmountOfHighestIndividualScore = getPlayersWithMostAmountOfHighestIndividualScore(
-    scoresByPlayer
-  );
-  players = players.filter((playerID, i) =>
-    playerIndecesWithMostAmountOfHighestIndividualScore.includes(i)
-  );
-  if (players.length === 1) {
-    return players;
-  }
-
-  // 5. players with second highest individual score
-  scoresByPlayer = scoresByPlayer.filter((playerScores, i) =>
-    playerIndecesWithMostAmountOfHighestIndividualScore.includes(i)
-  );
-  const playerIndecesWithSecondHighestIndividualScore = getPlayersWithSecondHighestIndividualScore(
-    scoresByPlayer
-  );
-  players = players.filter((playerID, i) =>
-    playerIndecesWithSecondHighestIndividualScore.includes(i)
-  );
-  if (players.length === 1) {
-    return players;
-  }
-
-  // otherwise: all left players win together
   return players;
-}
-
-/**
- *
- *
- * @param {Score[][]} scoresByPlayer
- * @returns {number[]} indeces of player cols with highest score
- */
-export function getPlayersWithHighestScore(
-  scoresByPlayer: Score[][]
-): number[] {
-  // 1. get player with highest score
-  const latestRow = scoresByPlayer.map(
-    (playerScores) => playerScores[playerScores.length - 1]
-  );
-  const highestScore = Math.max(...latestRow.map(({ total }) => total));
-
-  const playersWithHighestScore = latestRow
-    .map((score, i) => ({ i, score }))
-    .filter(({ score }) => score.total === highestScore)
-    .map(({ i }) => i);
-  if (playersWithHighestScore.length === 0) {
-    throw new Error("did not find any player with highest score");
-  }
-  return playersWithHighestScore;
-}
-
-export function getPlayersWithMostWonRounds(
-  scoresByPlayer: Score[][]
-): number[] {
-  // 2. get player with most won rounds
-  const wonRounds = scoresByPlayer.map((playerScores) =>
-    playerScores.reduce(
-      (sumWonRounds, score) =>
-        score.score > 0 ? sumWonRounds + 1 : sumWonRounds,
-      0
-    )
-  );
-
-  const maxWonRounds = Math.max(...wonRounds);
-  const playersWithMostWonRounds = wonRounds
-    .map((wins, i) => ({ wins, i }))
-    .filter(({ wins }) => wins === maxWonRounds)
-    .map(({ i }) => i);
-  if (playersWithMostWonRounds.length === 0) {
-    throw new Error("did not find any player with most won rounds");
-  }
-  return playersWithMostWonRounds;
-}
-
-export function getPlayersWithHighestIndividualScore(
-  scoresByPlayer: Score[][]
-): number[] {
-  // 3. get players with highest individual score
-  const highestIndividualScores = scoresByPlayer.map((playerScores) =>
-    Math.max(...playerScores.map((score) => score.score))
-  );
-
-  const maxHighestIndividualScore = Math.max(...highestIndividualScores);
-  const playersWithMaxHighestIndividualScore = highestIndividualScores
-    .map((score, i) => ({ score, i }))
-    .filter(({ score }) => score === maxHighestIndividualScore)
-    .map(({ i }) => i);
-  if (playersWithMaxHighestIndividualScore.length === 0) {
-    throw new Error("did not find any player with highest individual score");
-  }
-  return playersWithMaxHighestIndividualScore;
-}
-
-export function getPlayersWithMostAmountOfHighestIndividualScore(
-  scoresByPlayer: Score[][]
-): number[] {
-  const highestIndividualScores = scoresByPlayer.map((playerScores) =>
-    Math.max(...playerScores.map((score) => score.score))
-  );
-  const maxHighestIndividualScore = Math.max(...highestIndividualScores);
-
-  const amountsOfHighestIndividualScore = scoresByPlayer.map(
-    (playerScores) =>
-      playerScores.filter((score) => score.score === maxHighestIndividualScore)
-        .length
-  );
-  const maxAmountOfHighestIndividualScore = Math.max(
-    ...amountsOfHighestIndividualScore
-  );
-  const playersWithMostAmountOfHighestIndividualScore = amountsOfHighestIndividualScore
-    .map((amount, i) => ({ amount, i }))
-    .filter(({ amount }) => amount === maxAmountOfHighestIndividualScore)
-    .map(({ i }) => i);
-  if (playersWithMostAmountOfHighestIndividualScore.length === 0) {
-    throw new Error(
-      "did not find any player with most amount of highests individual score"
-    );
-  }
-  return playersWithMostAmountOfHighestIndividualScore;
-}
-
-export function getPlayersWithSecondHighestIndividualScore(
-  scoresByPlayer: Score[][]
-): number[] {
-  const highestIndividualScores = scoresByPlayer.map((playerScores) =>
-    Math.max(...playerScores.map((score) => score.score))
-  );
-  const maxHighestIndividualScore = Math.max(...highestIndividualScores);
-
-  const secondHighestIndividualScores = scoresByPlayer.map((playerScores) =>
-    Math.max(
-      ...playerScores
-        .map((score) => score.score)
-        .filter((score) => score !== maxHighestIndividualScore)
-    )
-  );
-  const maxSecondHighestIndividualScore = Math.max(
-    ...secondHighestIndividualScores
-  );
-
-  const playersWithSecondHighestIndividualScore = secondHighestIndividualScores
-    .map((score, i) => ({ score, i }))
-    .filter(({ score }) => score === maxSecondHighestIndividualScore)
-    .map(({ i }) => i);
-
-  if (playersWithSecondHighestIndividualScore.length === 0) {
-    throw new Error(
-      "did not find any player with second highest individual score"
-    );
-  }
-
-  return playersWithSecondHighestIndividualScore;
 }
 
 export function transposeMatrix<T>(matrix: T[][]): T[][] {
   // https://stackoverflow.com/a/17428705/5025424
-  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
+  return matrix[0].map((_col, i) => matrix.map((row) => row[i]));
 }
