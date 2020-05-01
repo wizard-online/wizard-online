9102cf981c448d97a397d135467df8d50bb69eac
janKir <9143877+janKir@users.noreply.github.com>
2020-05-02 01:38:06 +0200

    log verison to console

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 export const packageVersion = process.env.npm_package_version;
-export const gitVersion = process.env.GIT_VERSION;
+export const gitVersion = process.env.GIT_DESCRIBE;
 
 console.log(packageVersion, gitVersion);
