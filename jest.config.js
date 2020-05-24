281bf13ba9ba805f9d2eacd023ed4c3104688731
janKir <9143877+janKir@users.noreply.github.com>
2020-05-24 03:28:27 +0200

    retry fix jest in ci

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFiles: ["jest-localstorage-mock", "jest-date-mock"],
   setupFilesAfterEnv: [`<rootDir>/jest.setup.ts`],
-  transformIgnorePatterns: ["/*.mp3/"],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/test/mocks/fileMock.js",
+  },
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/.cache/",
