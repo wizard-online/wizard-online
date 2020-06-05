fa53d0981b31ea137302d966dbdd9001b59e6828
janKir <9143877+janKir@users.noreply.github.com>
2020-06-05 22:31:37 +0200

    use postgres storage if available

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,43 @@
+/* eslint-disable no-console */
 import * as Sentry from "@sentry/node";
 import { Server } from "boardgame.io/server";
+import { PostgresStore } from "bgio-postgres";
 import { loadGameConfig } from "./game/load-game-config";
 
 require("dotenv").config();
 
 Sentry.init({ dsn: process.env.SENTRY_SERVER_DSN });
 
+let db: PostgresStore | undefined;
+if (
+  process.env.DB_HOST &&
+  process.env.DB_NAME &&
+  process.env.DB_USER &&
+  process.env.DB_PASSWORD
+) {
+  console.log("using postgresql storage");
+
+  db = new PostgresStore({
+    database: process.env.DB_HOST,
+    username: "postgres",
+    password: "dthosen",
+    host: process.env.DB_HOST,
+  });
+} else {
+  console.log("using in-memory storage");
+}
+
 const server = Server({
   games: [loadGameConfig()],
+  db,
 });
 
 // Start the server
-// eslint-disable-next-line no-console
 try {
-  // eslint-disable-next-line no-console
   server.run(Number.parseInt(process.env.PORT || "8000", 10), () =>
-    // eslint-disable-next-line no-console
     console.log("server running...")
   );
 } catch (error) {
-  // eslint-disable-next-line no-console
   console.log(error);
   throw error;
 }
