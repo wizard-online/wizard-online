00c57201aa87aa1551e8ba8257bcdcad90bc037e
janKir <9143877+janKir@users.noreply.github.com>
2020-07-07 22:54:02 +0200

    disable turnAlert on error

--- src/ui/hooks/alert-client-turn.ts
+++ src/ui/hooks/alert-client-turn.ts
@@ -2,17 +2,22 @@ import { useCallback } from "react";
 import audioAlertFile from "../../assets/kalimba.mp3";
 import { useGameEventHandler } from "./game-event-handler";
 import { GameEvent } from "../util/game-events";
-import { useProfile } from "../ProfileProvider";
+import { useProfile, useProfileContext } from "../ProfileProvider";
 
 const audioAlert = new Audio(audioAlertFile);
 
 export function useAlertClientTurn(): void {
+  const { updateProfile } = useProfileContext();
   const profile = useProfile();
   const { turnAlert } = profile.preferences;
   const handleClientTurn = useCallback((): void => {
     if (turnAlert) {
-      audioAlert.play();
+      try {
+        audioAlert.play();
+      } catch (error) {
+        updateProfile({ preferences: { turnAlert: false } });
+      }
     }
-  }, [turnAlert]);
+  }, [turnAlert, updateProfile]);
   useGameEventHandler(GameEvent.ClientTurn, handleClientTurn);
 }
