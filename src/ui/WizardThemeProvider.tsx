1de65ca6baa3135f7fc0dff4ee1aad138f005505
janKir <9143877+janKir@users.noreply.github.com>
2020-10-05 20:42:27 +0200

    use character color as theme color

--- src/ui/WizardThemeProvider.tsx
+++ src/ui/WizardThemeProvider.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core";
 import { getWizardTheme } from "./util/mui-theme";
+import { useProfile } from "./ProfileProvider";
+import { characters } from "./util/character-theme";
 
 export const WizardThemeProvider: React.FC = ({ children }) => {
-  return <ThemeProvider theme={getWizardTheme()}>{children}</ThemeProvider>;
+  const profile = useProfile();
+  const primaryColor =
+    profile && profile.character
+      ? characters[profile.character]?.color
+      : undefined;
+  return (
+    <ThemeProvider theme={getWizardTheme(primaryColor)}>
+      {children}
+    </ThemeProvider>
+  );
 };
--- src/ui/util/mui-theme.ts
+++ src/ui/util/mui-theme.ts
@@ -1,11 +1,15 @@
 import { createMuiTheme, Theme } from "@material-ui/core";
-import { colors } from "./colors";
+import { colors, ColorTripleTone } from "./colors";
 
-export function getWizardTheme(primary: string = colors.wizard.green): Theme {
+export function getWizardTheme(
+  { medium: main, light, dark }: ColorTripleTone = colors.green
+): Theme {
   return createMuiTheme({
     palette: {
       primary: {
-        main: primary,
+        main,
+        light,
+        dark,
       },
     },
   });
