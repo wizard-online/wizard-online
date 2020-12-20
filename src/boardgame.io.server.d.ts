2dcff46af034e483cbbb3860caa7b8464f00e7f2
janKir <9143877+janKir@users.noreply.github.com>
2020-12-20 12:56:56 +0100

    delete unecessary types

--- src/boardgame.io.server.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "boardgame.io/server" {
-  export interface Server {
-    run(port: number, onStart: () => void): Promise<unknown>;
-    kill(args: unknown): void;
-  }
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  export function Server(options: object): Server;
-}
