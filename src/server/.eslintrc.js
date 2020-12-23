1ea315aaf2a6f83bf40df01345b7456edc040bad
janKir <9143877+janKir@users.noreply.github.com>
2020-12-23 01:50:24 +0100

    fix eslint settings for server code

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    jsx: false,
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname
+  },
   rules: {
     "no-console": "off"
   }
